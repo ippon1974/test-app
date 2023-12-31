6d96ee85dd3361043b5fcd45f7ac3d2ee1fd0b27
Vovan <plategi@yandex.ru>
2023-12-31 14:16:31 +0200

    add

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,4 @@
 <h1>Не бойтесь нас мы негры</h1>
 
 <a href="{{url('/')}}" title="home">Welcom</a>
+<a href="{{url('/hellocontroller')}}">Hello</a>
--- resources/views/profile/hello.blade.php
+++ resources/views/profile/hello.blade.php
@@ -1,1 +1,2 @@
 <h1>Hello this is view</h1>
+<a href="{{url('/about')}}" title="Негры">Негры</a>
