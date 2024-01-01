fa94c9c27aef57e44baf9f7cba8906f84056b627
Vovan <plategi@yandex.ru>
2024-01-01 12:11:23 +0200

    o

--- app/Http/Controllers/helloController.php
+++ app/Http/Controllers/helloController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class helloController extends Controller
 {
     public function hello(){
+        echo $helloController = Post::create([
+            'name' => 'Rehan Khan',
+            'age' => '55',
+            'uin' => '100'
+        ]);
         return view('profile.hello');
+
     }
 }
