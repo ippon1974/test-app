13c671af2e572864dce0ae4d9d6b3f671f73cff9
Vovan <plategi@yandex.ru>
2024-01-04 15:41:46 +0200

    o

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Вторичное жилье. Новостройки. Ипотека. Москва, Московская область.</title>
+    <meta name="description" content="Новостройи и вторичное жилье в Москве и Московской области">
+    <meta name="keywords" content="Новостройки, вторичка, аредна, покупака, обмен">
 </head>
 <body>
     <h1>Вторичное жилье. Новостройки. Ипотека. Москва, Московская область.</h1>
